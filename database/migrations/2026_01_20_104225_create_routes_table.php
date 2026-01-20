d3d91b3d55cf0bfcb8d26aedbd6f44b939dce919
vayd0 <kiiroxyz@gmail.com>
2026-01-20 12:02:44 +0100

    Itialisation

---
+++ database/migrations/2026_01_20_104225_create_routes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id('route_id');
+            $table->string('route_short_name');
+            $table->string('route_long_name');
+            $table->text('route_desc')->nullable();
+            $table->integer('route_type');
+            $table->string('route_url')->nullable();
+            $table->string('route_color', 6)->nullable();
+            $table->string('route_text_color', 6)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes');
+    }
+};
